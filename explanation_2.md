fa5f2c2a1a89bf026aef84996eaad2b1fb15c462
Denis Moroz <denis.moroz@toptal.com>
2020-11-11 11:16:06 +0300

    Problem 2 with explanation

---
+++ explanation_2.md
@@ -0,0 +1,18 @@
+
+### Search in a Rotated Sorted Array
+
+As input list is sorted, binary search algorithm can be applied on input array with a slight modification. 
+
+While dividing input array on two parts by middle element one of the part 
+should be sorted correctly, first element should be less then last one and the other should not.
+If part is sorted it is easy to check if search element can be found inside that part,
+if yes, then use regular binary search algorithm to find out position, if no skipp it.
+
+Then run recursively algorithm again on part that looks unsorted.
+  
+
+### Time and Space complexity
+
+Space complexity: 0(n).
+Time complexity: O(log n).
+ 
--- problem_2.py
+++ problem_2.py
@@ -7,21 +7,30 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
-    return search(input_list, number, 0, len(input_list) -1)
+
+    if not isinstance(input_list, list):
+        raise ValueError("Input list should be a list")
+
+    if not isinstance(number, int):
+        raise ValueError("Number should be integer")
+
+    return search(input_list, number, 0, len(input_list) - 1)
 
 
 def search(input_list, number, start, end):
-    if end - start == 1 or start == end:
+
+    if len(input_list) == 0:
+        return -1
+
+    if end - start == 1:
         if input_list[start] == number:
             return start
         if input_list[end] == number:
             return end
-
         return -1
 
     middle = start + (end - start) // 2
 
-    # start, middle
     start_item = input_list[start]
     middle_item = input_list[middle]
     end_item = input_list[end]
@@ -29,7 +38,7 @@ def search(input_list, number, start, end):
     res = -1
 
     if start_item <= middle_item:
-        if start_item <= number <= middle_item:
+        if start_item <= number < middle_item:
             return search(input_list, number, start, middle)
     else:
         res = search(input_list, number, start, middle)
@@ -37,8 +46,8 @@ def search(input_list, number, start, end):
     if res != -1:
         return res
 
-    if middle_item <= end_item:
-        if middle_item <= number <= end_item:
+    if middle_item < end_item:
+        if middle_item < number < end_item:
             return search(input_list, number, middle, end)
     else:
         res = search(input_list, number, middle, end)
@@ -62,8 +71,29 @@ def test_function(test_case):
         print("Fail")
 
 
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+test_function([[1, 3], 1])  # Pass
+test_function([[3, 1], 1])  # Pass
+test_function([[4, 2], 2])  # Pass
+test_function([[2, 4], 2])  # Pass
+test_function([[2, 3, 4, 1], 1])  # Pass
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])  # Pass
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])  # Pass
+test_function([[6, 7, 8, 1, 2, 3, 4], 8])  # Pass
+test_function([[6, 7, 8, 1, 2, 3, 4], 1])  # Pass
+test_function([[6, 7, 8, 1, 2, 3, 4], 10])  # Pass
+
+# Edge cases
+
+# Search on empty list
+test_function([[], 10])  # Pass
+
+try:
+    rotated_array_search([1], None)
+except ValueError as e:
+    print(e)  # number should be integer
+
+try:
+    rotated_array_search(None, 10)
+except ValueError as e:
+    print(e)  # input list should be a list
+
